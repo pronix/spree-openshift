1b471e33fad8a81e1ec84545d449985c1fb801ae
dima <pronix.service@gmail.com>
2018-01-01 11:15:51 +0100

    not use generators

--- migrate-database.sh
+++ migrate-database.sh
@@ -2,7 +2,5 @@
 set -e
 
 echo "Executing bundle exec 'rake db:migrate' ..."
-RAILS_ENV=production bundle exec rails g spree:install --user_class=Spree::User
-RAILS_ENV=production bundle exec rails g spree:auth:install
-RAILS_ENV=production bundle exec rails g spree_gateway:install
+RAILS_ENV=production bundle exec rake railties:install:migrations
 RAILS_ENV=production bundle exec rake db:migrate
